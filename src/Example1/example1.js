f9e97e5e214f596c5cd81c9e9a10246c74bd3c2f
Pramod <pramodmithyantha@gmail.com>
2019-07-12 09:04:53 +0530

    css1

---
+++ src/Example1/example1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './example.css';
+
+function Css1(){
+  return(
+    <div className="grid1">
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+      <div>1</div>
+    </div>
+  )
+}
+
+export default Css1;
